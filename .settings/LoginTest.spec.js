eda9c3be2d639095d14230d79a40c8d2fddaabd3
Sivareddy <117708670+Allu1232@users.noreply.github.com>
2025-12-15 13:19:37 +0530

    updated .settings/LoginTest.spec.js

---
+++ .settings/LoginTest.spec.js
@@ -0,0 +1,10 @@
+describe('Login Functionality', () => {
+  it('should log in with valid credentials', () => {
+    cy.visit('/login');
+    cy.get('input[name="username"]').type('testUser');
+    cy.get('input[name="password"]').type('testPassword');
+    cy.get('button[type="submit"]').click();
+    cy.url().should('include', '/dashboard');
+    cy.get('.welcome-message').should('contain', 'Welcome, testUser');
+  });
+});
