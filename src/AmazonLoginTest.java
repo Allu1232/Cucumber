0586c57d8e6b8ead4dc1e64a57be63a98cc16750
Sivareddy <117708670+Allu1232@users.noreply.github.com>
2025-12-23 13:04:37 +0530

    updated src/AmazonLoginTest.java

--- src/AmazonLoginTest.java
+++
@@ -1,5 +0,0 @@
-cy.get('#username').type('validUser');
-cy.get('#password').type('validPassword');
-cy.get('#loginButton').click();
-cy.url().should('include', '/dashboard');
-cy.get('.welcome-message').should('contain', 'Welcome, validUser');
